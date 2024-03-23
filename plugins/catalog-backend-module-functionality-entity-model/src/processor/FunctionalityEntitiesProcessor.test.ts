3a7e5d2fbfde9cb825045242d94995f6be47db35
suniljose <bsuniljose25@gmail.com>
2024-03-24 00:15:51 +0530

    Fixing the Processor tests

--- plugins/catalog-backend-module-functionality-entity-model/src/processor/FunctionalityEntitiesProcessor.test.ts
+++ plugins/catalog-backend-module-functionality-entity-model/src/processor/FunctionalityEntitiesProcessor.test.ts
@@ -1,20 +1,18 @@
-import { TemplateEntityV1beta3 } from '@backstage/plugin-scaffolder-common';
+import { FunctionalityEntityV1alpha1 } from '@internal/backstage-plugin-functionality-common';
 import { FunctionalityEntitiesProcessor } from './FunctionalityEntitiesProcessor';
 
 const mockLocation = { type: 'a', target: 'b' };
-const mockEntity: TemplateEntityV1beta3 = {
-  apiVersion: 'scaffolder.backstage.io/v1beta3',
-  kind: 'Template',
+const mockEntity: FunctionalityEntityV1alpha1 = {
+  apiVersion: 'functionality.tw/v1alpha1',
+  kind: 'Functionality',
   metadata: { name: 'n' },
   spec: {
-    parameters: {},
-    steps: [],
-    type: 'service',
     owner: 'o',
+    components: 'comp1'
   },
 };
 
-describe('ScaffolderEntitiesProcessor', () => {
+describe('FunctionalityEntitiesProcessor', () => {
   describe('validateEntityKind', () => {
     it('validates the entity kind', async () => {
       const processor = new FunctionalityEntitiesProcessor();
@@ -42,21 +40,37 @@ describe('ScaffolderEntitiesProcessor', () => {
 
       await processor.postProcessEntity(mockEntity, mockLocation, emit);
 
-      expect(emit).toHaveBeenCalledTimes(2);
+      expect(emit).toHaveBeenCalledTimes(4);
       expect(emit).toHaveBeenCalledWith({
         type: 'relation',
         relation: {
           source: { kind: 'Group', namespace: 'default', name: 'o' },
           type: 'ownerOf',
-          target: { kind: 'Template', namespace: 'default', name: 'n' },
+          target: { kind: 'Functionality', namespace: 'default', name: 'n' },
         },
       });
       expect(emit).toHaveBeenCalledWith({
         type: 'relation',
         relation: {
-          source: { kind: 'Template', namespace: 'default', name: 'n' },
+          source: { kind: 'Functionality', namespace: 'default', name: 'n' },
           type: 'ownedBy',
           target: { kind: 'Group', namespace: 'default', name: 'o' },
+        },
+      });
+      expect(emit).toHaveBeenCalledWith({
+        type: 'relation',
+        relation: {
+          source: { kind: 'Component', namespace: 'default', name: 'comp1' },
+          type: 'isPartOf',
+          target: { kind: 'Functionality', namespace: 'default', name: 'n' },
+        },
+      });
+      expect(emit).toHaveBeenCalledWith({
+        type: 'relation',
+        relation: {
+          source: { kind: 'Functionality', namespace: 'default', name: 'n' },
+          type: 'hasPartOf',
+          target: { kind: 'Component', namespace: 'default', name: 'comp1' },
         },
       });
     });
