b625363ef9ad1787001ba628e2fbd98c9f7dea29
suniljose <bsuniljose25@gmail.com>
2024-04-07 16:10:35 +0530

    Add on click functionality for the graph nodes in org-sturcture

--- plugins/org-structure/src/components/OrgTreeViewPage/OrgTreeViewPage.tsx
+++ plugins/org-structure/src/components/OrgTreeViewPage/OrgTreeViewPage.tsx
@@ -1,32 +1,67 @@
 import { Content, ContentHeader, DependencyGraph, DependencyGraphTypes, Header, InfoCard, Page } from "@backstage/core-components";
-import React from "react";
-import { useApi } from "@backstage/core-plugin-api";
-import { catalogApiRef } from "@backstage/plugin-catalog-react";
+import React, { useCallback } from "react";
+import { useApi, useRouteRef } from "@backstage/core-plugin-api";
+import { catalogApiRef, entityRouteRef } from "@backstage/plugin-catalog-react";
 import { useAsync } from 'react-use';
-import { GroupEntity } from "@backstage/catalog-model";
+import { DEFAULT_NAMESPACE, GroupEntity, parseEntityRef } from "@backstage/catalog-model";
+import { CustomRenderNode, EntityCustomNodeData } from "./CustomNode";
+import { useNavigate } from "react-router-dom";
+
+export type CustomEntityNode = DependencyGraphTypes.DependencyNode<EntityCustomNodeData>;
+
+
 
 export function OrgTreeViewPage() {
 
     const catalogApi = useApi(catalogApiRef);
-    const entitiesData = useAsync(async () => catalogApi.queryEntities({filter: {kind: 'Group'}}), []);
+    const entitiesData = useAsync(async () => catalogApi.queryEntities({ filter: { kind: 'Group' } }), []);
 
     // should we create state for these?
-    let nodes: DependencyGraphTypes.DependencyNode[] = [];
+    let nodes: DependencyGraphTypes.DependencyNode<EntityCustomNodeData>[] = [];
     let edges: DependencyGraphTypes.DependencyEdge[] = [];
+
+    const catalogEntityRoute = useRouteRef(entityRouteRef);
+    const navigate = useNavigate();
+    const onNodeClick = useCallback(
+        (node: CustomEntityNode, _: React.MouseEvent) => {
+            console.log({node});
+            const nodeEntityName = parseEntityRef({
+                kind: node.entity.kind,
+                namespace: node.entity.metadata.namespace || DEFAULT_NAMESPACE,
+                name: node.entity.metadata.name,
+            });
+            console.log({nodeEntityName})
+            const path = catalogEntityRoute({
+                kind: nodeEntityName.kind.toLocaleLowerCase('en-US'),
+                namespace: nodeEntityName.namespace.toLocaleLowerCase('en-US'),
+                name: nodeEntityName.name,
+            });
+            console.log(path);
+            navigate(path);
+        },
+        [catalogEntityRoute],
+    );
 
     if (!entitiesData.loading) {
         if (entitiesData.value?.items !== undefined) {
             const entities: GroupEntity[] = entitiesData.value?.items as GroupEntity[];
-            for(const entity of entities) {
-                nodes.push({id: entity.metadata.name});
-                for(const child of entity.spec.children) {
-                    edges.push({from: entity.metadata.name, to: child});
+            for (const entity of entities) {
+                const node: CustomEntityNode = {
+                    id: entity.metadata.name,
+                    entity: entity,
+                    name: entity.metadata.name,
+                    title: entity.metadata.title,
+                };
+                node.onClick = (event: React.MouseEvent<HTMLElement>) => onNodeClick(node, event);
+
+                nodes.push(node);
+                for (const child of entity.spec.children) {
+                    edges.push({ from: entity.metadata.name, to: child });
                 }
             }
         }
     }
 
-    
     return (
         <Page themeId="tool">
             <Header title="Org Structure" />
@@ -36,6 +71,7 @@ export function OrgTreeViewPage() {
                     <DependencyGraph
                         nodes={nodes}
                         edges={edges}
+                        renderNode={CustomRenderNode}
                     />
                 </InfoCard>
             </Content>
